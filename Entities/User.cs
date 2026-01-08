1f24837bb02cea3770ddffa4dd8a89db195e355e
Qasem <q.hashemi100@gmail.com>
2026-01-08 10:51:24 +0100

    Add Db Configuration

---
+++ Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace UserProfile.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string firstname { get; set; } = string.Empty;
+        public string lastname { get; set; } = string.Empty;
+        public string email { get; set; } = string.Empty;
+        public string password { get; set; }
+
+    }
+}
